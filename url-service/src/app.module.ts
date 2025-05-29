7f0eae1d4fbb04e4b494c06df3bbe76c936a997b
ltphat2204 <ltphat2204@gmail.com>
2025-05-30 01:24:47 +0700

    Setup project

---
+++ url-service/src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { LibsModule } from './libs/libs.module';
+
+@Module({
+  imports: [LibsModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
